eb9d127bd9d1332d48810e8cc2fb88694ad8de8d
Zain Shaikh <szain2024@gmail.com>
2024-07-27 20:03:09 +0530

    testing

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://secure-vision-global-v2.onrender.com',
     credentials: true,
 }))
 
