25db61158fdfd14c0380337d30c58ddd6565f6d5
Zain Shaikh <szain2024@gmail.com>
2024-08-06 15:21:37 +0530

    Url Changed

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
         "http://localhost:5173",
-      "https://svgfrontend-94kg.onrender.com",
+      "https://securevisionglobal.com",
     ],
     credentials: true,
   })
